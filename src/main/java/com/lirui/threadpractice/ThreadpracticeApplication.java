543a3bad7cc75be28289070db8dc57eed631ac31
lirui <630240701@qq.com>
2019-09-05 16:17:04 +0800

    wahaha

---
+++ src/main/java/com/lirui/threadpractice/ThreadpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.lirui.threadpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadpracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadpracticeApplication.class, args);
+    }
+
+}
